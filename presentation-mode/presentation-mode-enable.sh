423fd1db9a9c428544046407f0976c4987b08d41
alexkcal <alexk@berkeley.edu>
2015-10-19 13:54:25 -0700

    Update presentation-mode-enable.sh

--- presentation-mode/presentation-mode-enable.sh
+++ presentation-mode/presentation-mode-enable.sh
@@ -56,14 +56,6 @@ echo "Write value in plist for presentationmode to be enabled"
 echo "Run recon to update inventory for EA and Smart Groups"
 /usr/local/bin/jamf recon
 
-# Message to user that the Mac is in Presentation Mode and to disable in SS or auto-disable in 24 hours
-/usr/bin/osascript <<-EOF
-			    tell application "System Events"
-			        activate
-			        display dialog "Mac is now in Presentation Mode. Please disable Presentation Mode using Self Service when you are done. Presentation Mode will be automatically disabled in 24 hours." buttons {"OK"} default button 1
-			    end tell
-			EOF
-
 # Create launchdaemon to automatically disable presentation mode at desired time limit
 echo "Create the launchdaemon to disable presentation mode"
 echo "<?xml version="1.0" encoding="UTF-8"?> 
@@ -105,6 +97,14 @@ su -l "$loggedInUser" -c "defaults -currentHost write com.apple.screensaver idle
 
 # Kill cfprefsd to apply new settings
 /usr/bin/killall cfprefsd
+
+# Message to user that the Mac is in Presentation Mode and to disable in SS or auto-disable in 24 hours
+/usr/bin/osascript <<-EOF
+			    tell application "System Events"
+			        activate
+			        display dialog "Mac is now in Presentation Mode. Please disable Presentation Mode using Self Service when you are done. Presentation Mode will be automatically disabled in 24 hours." buttons {"OK"} default button 1
+			    end tell
+			EOF
 
 echo "Presentation Mode set to enabled. Launchdaemon created to automatically disable Presentation Mode in $disableTime seconds"
 
